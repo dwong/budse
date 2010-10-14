509a39b7f39aed11416acba85149ca17bad8e7f7
Derek Wong <derek@goingthewongway.com>
2010-10-14 04:23:07 +0000

    Refactor to use ternary operator

--- budsecli.py
+++ budsecli.py
@@ -415,10 +415,7 @@ class BudseCLI(object):
                 return
             except budse.DuplicateException, e:
                 clear_screen()
-                if (len(e.duplicates)==1):
-                    plural = ''
-                else:
-                    plural = 's'
+                plural = '' if (len(e.duplicates)==1) else 's'
                 print('%s:\n== Duplicate%s ==\n' % (e, plural))
                 self.output_transactions(e.duplicates)
                 if self._confirm('Ignore duplicate%s?' % plural, False):
