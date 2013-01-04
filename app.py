42cf06b8fd82e5be081a81fce8cb14917edc5c96
Derek Wong <derekpublic@gmail.com>
2013-01-03 22:34:16 -0800

    Use default PostgreSQL for local

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql://derek:@localhost/postgresql')
 db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'NO WAY'
+
+@app.route('/go_there')
+def bye():
+    return 'suck'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
