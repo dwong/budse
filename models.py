0358fca46dc4acfc11d8abe238969a6df8aa2547
Derek Wong <derekpublic@gmail.com>
2013-01-05 23:26:32 -0800

    Transaction model

--- models.py
+++ models.py
@@ -169,6 +169,164 @@ class AutoDeposit(db.Model):
                  self.amount, delimiter, self.percentage_or_fixed, delimiter,
                  'Gross' if self.affect_gross else 'Net', delimiter, self.active))
 
+class Transaction(db.Model):
+    """Base class for all transactions."""
+
+    # Pseudo static class variables that also have meaning in the database
+    DEPOSIT = '+'
+    WITHDRAWAL = '-'
+    DEDUCTION = '|'
+    INFORMATIONAL = '?'
+    TRANSFER = '='
+
+    __tablename__ = 'transactions'
+
+    id = Column(Integer, primary_key=True)
+    _timestamp = Column('timestamp', DateTime)
+    date = Column(Date)
+    _user = Column('user_id', Integer, ForeignKey('users.id'))
+    _account = Column('account_id', Integer, ForeignKey('accounts.id'))
+    account_name = Column(String)
+    _amount = Column('amount', Integer)
+    description = Column(String)
+    action = Column(String)
+    tags = Column(String)
+    _parent = Column('parent_id', Integer, ForeignKey('transactions.id'))
+    _active = Column('active', Boolean, default=False)
+    __mapper_args__ = {'polymorphic_on':action,
+                       'polymorphic_identity':INFORMATIONAL}
+
+    user = relation('User', backref=backref('transactions', order_by=id))
+    account = relation('Account', backref=backref('transactions', order_by=id))
+    children = relation('Transaction', primaryjoin=_parent == id, cascade='all',
+                        backref=backref('parent', remote_side=id),
+                        lazy='joined', join_depth=1)
+    initial = False
+
+    def __init__(self, date, user, description=None, amount=0.00, account=None,
+                 parent=None, duplicate_override=False):
+        self.date = date
+        self.user = user
+        self.account = account
+        self.amount = amount
+        self.description = description
+        self.parent = parent
+        self.initial = True
+        self.timestamp = datetime.datetime.now()
+
+        if not duplicate_override:
+            duplicates = []
+            for t in session.query(Transaction).\
+                filter(or_(Transaction.parent != parent,
+                           Transaction.parent == None)).\
+                filter(Transaction.action != Transaction.TRANSFER).\
+                filter(Transaction.action != Transaction.DEDUCTION).\
+                filter(Transaction.action != Transaction.INFORMATIONAL).\
+                filter(Transaction.date == date).\
+                filter(Transaction.status == True).\
+                filter(Transaction.account == account).\
+                filter(Transaction.amount ==
+                       _format_db_amount(amount)).all():
+                duplicates.append(t)
+            if len(duplicates) > 0:
+                if len(duplicates) == 1:
+                    error = 'Possible duplicate found'
+                else:
+                    error = 'Possible duplicates found'
+                raise DuplicateException(error, duplicates)
+
+    def _set_amount(self, amount):
+        self._amount = _format_db_amount(amount)
+    def _get_amount(self):
+        return _format_out_amount(self._amount)
+    amount = synonym('_amount', descriptor=property(_get_amount, _set_amount))
+
+    def _set_timestamp(self, ts):
+        self._timestamp = ts
+    def _get_timestamp(self):
+        ts = self._timestamp
+        return('%s-%02d-%02d %02d:%02d:%02d' % (ts.year, ts.month, ts.day,
+                                                ts.hour, ts.minute, ts.second))
+    timestamp = synonym('_timestamp', descriptor=property(_get_timestamp,
+                                                          _set_timestamp))
+
+    def commit(self):
+        """Only used for the first save to the database."""
+        if not self.active:
+            ## TODO: self.initial seems unnecessary
+            if self.parent is None and self.initial:
+                self._status = True
+                self.initial = False
+                for t in self.children:
+                    t.commit()
+            else: # This comes from the transaction's parent
+                self._status = True
+                for t in self.children: # Maybe support multiple nesting in the future?
+                    t.commit()
+
+    def _get_active(self):
+        return self._active
+    def _set_active(self, active):
+        if active != self._active:
+            self._undo_action(active)
+            for t in self.children:
+                t._undo_action(active)
+    active = synonym('_active', descriptor=property(_get_active, _set_active))
+
+    def _undo_action(self, active):
+        if self.account is not None:
+            if (self.action == Transaction.DEPOSIT and active) or \
+                   (self.action == Transaction.WITHDRAWAL and not active):
+                self.account.total += self.amount
+            elif(self.action == Transaction.WITHDRAWAL and active) or \
+                   (self.action == Transaction.DEPOSIT and not active):
+                self.account.total -= self.amount
+        self._active = active
+
+    def __str__(self):
+        delimiter = '\n'
+        if self.action == Transaction.DEPOSIT:
+            action_type = 'Deposit'
+        elif self.action == Transaction.WITHDRAWAL:
+            action_type = 'Withdrawal'
+        elif self.action == Transaction.DEDUCTION:
+            action_type = 'Deduction'
+        elif self.action == Transaction.TRANSFER:
+            action_type = 'Transfer'
+        else:
+            action_type = 'Informational'
+
+        if self.account is not None:
+            account = self.account.name
+        else:
+            account = 'Whole account'
+
+        deduction_repr = subdeposit_repr = ''
+        if self.parent is None:
+            deduction_repr = subdeposit_repr = subwithdrawal_repr = ''
+            for deduction in session.query(Deduction).\
+                    filter(Deduction.parent == self).all():
+                deduction_repr += ('(Amount: %s, Description: %s)' %
+                                   (deduction.amount, deduction.description))
+            else:
+                deduction_repr = 'Deductions: [%s]%s' % (deduction_repr,
+                                                         delimiter)
+            for deposit in session.query(Deposit).\
+                    filter(Transaction.parent == self).all():
+                subdeposit_repr += ('(Account: %s, Amount: %s,'
+                                    'Description: %s)' %
+                                    (deposit.account.name, deposit.amount,
+                                     deposit.description))
+            else:
+                subdeposit_repr = 'Deposits: [%s]%s' % (subdeposit_repr,
+                                                         delimiter)
+        return('Type: %s%sAmount: $%0.2f%sTransaction Date: %s%sAccount: '
+               '%s%sDescription: %s%s%s%sActive: %s' %
+               (action_type, delimiter, self.amount, delimiter,
+                self.date.strftime('', delimiter), account, delimiter,
+                self.description, delimiter, deduction_repr, subdeposit_repr, 
+                self.active))
+
 def _format_db_amount(amount):
     return int(round(float(amount) * 100))
 
