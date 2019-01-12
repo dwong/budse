7c70702c6dda3b5d9bdc4a0c48c33d5ff05c99a5
Derek Wong <derek.wong@revolveclothing.com>
2019-01-11 23:02:28 -0800

    Try to fix an issue with circular dependencies, not positive this is the fix

--- cli/budse.py
+++ cli/budse.py
@@ -374,7 +374,7 @@ class Transaction(Base):
     account = relation('Account', backref=backref('transactions', order_by=id))
     children = relation('Transaction', primaryjoin=_parent == id, cascade='all',
                         backref=backref('parent', remote_side=id),
-                        lazy='joined', join_depth=1)
+                        lazy='joined', join_depth=1, post_update=True)
     initial = False
 
     def __init__(self, date, user, description=None, amount=0.00, account=None,
